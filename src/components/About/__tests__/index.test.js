a1e6bc8b06c979df57e8d7d90354eee211cb0a3e
Ronya <klmirg6@gmail.com>
2022-02-22 01:21:42 -0600

    added more

--- src/components/About/__tests__/index.test.js
+++ src/components/About/__tests__/index.test.js
@@ -15,6 +15,7 @@ describe('About component', () => {
   // Second Test
   it('matches snapshot DOM node structure', () => {
     const { asFragment } = render(<About />);
+    
     expect(asFragment()).toMatchSnapshot();
   })
 })
