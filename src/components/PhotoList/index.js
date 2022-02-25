dbf47fe0eb2c38de1f834226688a1c1b3628823b
Ronya <klmirg6@gmail.com>
2022-02-24 19:20:47 -0600

    modals are closing, next is tests

--- src/components/PhotoList/index.js
+++ src/components/PhotoList/index.js
@@ -107,12 +107,12 @@ const PhotoList = ({ category }) => {
 
   const toggleModal = (image, i) => {
     setCurrentPhoto({...image, index: i})
-    setIsModalOpen(true);
+    setIsModalOpen(!isModalOpen);
   }
 
   return (
     <div>
-      {isModalOpen && <Modal currentPhoto={currentPhoto}></Modal>}
+      {isModalOpen && (<Modal currentPhoto={currentPhoto} onClose={toggleModal}></Modal>)}
       <div className="flex-row">
         {currentPhotos.map((image, i) => (
           <img
